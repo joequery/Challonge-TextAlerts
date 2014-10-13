96fdd090f4fab9f50453ca32f6792e9b299219b1
Joseph McCullough <joseph@vertstudios.com>
2014-10-13 06:29:14 -0500

    progress on options page

--- assets/js/options.js
+++ assets/js/options.js
@@ -4,13 +4,15 @@ var $twilio_form = document.querySelector('#twilio');
 var $twilio_api_input = document.querySelector('#twilio_api');
 var $twilio_url_input = document.querySelector('#twilio_url');
 var $key_visibility_btn = document.querySelector('#api_key_visibility');
-var $save = document.querySelector('#save');
+var $save_gateway_info = document.querySelector('#save_gateway');
 var $status_text = document.querySelector('#status');
 var $clear_phones_btn = document.querySelector('#clear_phones');
+var $storage = document.querySelector('#storage_usage');
 
 var api_visible = false;
 
-$key_visibility_btn.addEventListener('click', function(){
+$key_visibility_btn.addEventListener('click', function(evt){
+    evt.preventDefault();
     if(api_visible){
         this.textContent = '(show)';
         $twilio_api_input.setAttribute('type', 'password');
@@ -23,7 +25,7 @@ $key_visibility_btn.addEventListener('click', function(){
     }
 });
 
-$save.addEventListener('click', function(){
+$save_gateway_info.addEventListener('click', function(){
     STORAGE.set({'ACCESS_TOKEN': $twilio_api_input.value}, function(){
         $status_text.textContent = 'Access token saved. ';
         STORAGE.set({'GATEWAY_URL': $twilio_url_input.value}, function(){
@@ -40,6 +42,7 @@ $clear_phones_btn.addEventListener('click', function(){
         STORAGE.set({'ACCESS_TOKEN': $twilio_api_input.value}, function(){
             STORAGE.set({'GATEWAY_URL': $twilio_url_input.value});
             $status_text.textContent = 'Phone numbers cleared';
+            get_storage_usage();
         });
     });
 });
@@ -51,5 +54,15 @@ STORAGE.get(['ACCESS_TOKEN', 'GATEWAY_URL'], function(data){
     $twilio_api_input.value = twilio_api;
     $twilio_url_input.value = twilio_url;
 });
+
+var get_storage_usage = (function f(){
+    STORAGE.getBytesInUse(null, function(bytes){
+        var percentage = bytes / STORAGE.QUOTA_BYTES;
+        percentage = Math.round(percentage * 100) / 100;
+        var info = '(' + bytes + ' / ' + STORAGE.QUOTA_BYTES + '): ' + percentage + '%';
+        $storage.textContent = info;
+    });
+    return f;
+})();
 
 })();
